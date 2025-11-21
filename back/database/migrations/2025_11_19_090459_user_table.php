6a42ececad898792dd2de77bd7c371f054b7d2e1
Ian <iaancastellanos1234@gmail.com>
2025-11-21 10:01:26 +0100

    Prueba

--- back/database/migrations/2025_11_19_090459_user_table.php
+++ back/database/migrations/2025_11_19_090459_user_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
 
-            $table->uuid('id')->primary()->default(DB::raw('UUID()'));
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
 
             // Datos personales
             $table->string('name');
