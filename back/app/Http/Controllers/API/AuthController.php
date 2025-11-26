cbee3ac3b14dd1a61782016bfcf152177b6ec78e
Rmaon <ramoncamineroarroyo@gmail.com>
2025-11-26 17:56:09 +0100

    feature/Mantener inicio sesión iniciada

--- back/app/Http/Controllers/API/AuthController.php
+++ back/app/Http/Controllers/API/AuthController.php
@@ -6,12 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;   
+use Illuminate\Support\Facades\Validator;
 
 
-class AuthController extends Controller{
-    public function register(Request $request){
-	    $input = $request->all();
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $input = $request->all();
         $rules = [
             'name' => 'required|string|max:20',
             'email' => 'required|email|max:255|unique:users',
@@ -24,36 +26,32 @@ class AuthController extends Controller{
             'same' => 'El campo :attribute y :other deben coincidir.',
             'max' => 'El campo :attribute no debe exceder el tamaño máximo permitido.',
             'min' => 'El campo :attribute debe tener al menos :min caracteres.',
-            'between' => 'El campo :attribute debe estar entre :min y :max años.',
-            'in' => 'El campo :attribute debe ser uno de los siguientes valores: :values.',
-            //'integer' => 'El campo :attribute debe ser un número entero.',
             'required' => 'El campo :attribute es obligatorio.'
         ];
 
         $validator = Validator::make($request->all(), $rules, $messages);
-        if($validator->fails()){
-            return response()->json($validator->errors(),422);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
         }
-
 
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
 
-        //$tokenResult = $user->createToken('LaravelSanctumAuth');
-
-        // Actualizar expiración
-        // $hours = (int) env('SANCTUM_EXPIRATION_HOURS', 2);
-        // $tokenResult->accessToken->expires_at = now()->addHours($hours);
-        // $tokenResult->accessToken->save();
+        $tokenResult = $user->createToken('LaravelSanctumAuth', ['usuario']);
+        $tokenResult->accessToken->save();
 
         $success = [
             'id' => $user->id,
             'name' => $user->name,
-            //'token' => $user->createToken('LaravelSanctumAuth')->plainTextToken,
-            //'expires_at' => $tokenResult->accessToken->expires_at ==null ? null:  $tokenResult->accessToken->expires_at->toDateTimeString()
+            'token' => $tokenResult->plainTextToken, // ← NUEVO
+            'expires_at' => $tokenResult->accessToken->expires_at == null ? null : $tokenResult->accessToken->expires_at->toDateTimeString()
         ];
-        
-        return response()->json(["success"=>true,"data"=>$success, "message" => "User successfully registered!"]);
+
+        return response()->json([
+            "success" => true,
+            "data" => $success,
+            "message" => "Usuario registrado correctamente"
+        ]);
     }
 
     public function login(Request $request)
@@ -61,7 +59,7 @@ class AuthController extends Controller{
 
         $input = $request->all();
         $rules = [
-            'name' => 'required|string|max:20',
+            'name' => 'required|string',
             'password' => 'required|min:8'
         ];
         $messages = [
@@ -71,16 +69,16 @@ class AuthController extends Controller{
         ];
 
         $validator = Validator::make($input, $rules, $messages);
-        if($validator->fails()){
-            return response()->json($validator->errors(),422);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
         }
 
-        if (str_contains($request->name,'@') && str_contains($request->name,'.')) {
-            if(Auth::attempt(['email' => $request->name, 'password' => $request->password])){
+        if (str_contains($request->name, '@') && str_contains($request->name, '.')) {
+            if (Auth::attempt(['email' => $request->name, 'password' => $request->password])) {
                 $auth = Auth::user();
                 // return $auth;
                 //$tokenResult = $auth->createToken('LaravelSanctumAuth');
-                $tokenResult = $auth->createToken('LaravelSanctumAuth',['usuario']); // Asignar abilities al token
+                $tokenResult = $auth->createToken('LaravelSanctumAuth', ['usuario']); // Asignar abilities al token
 
                 // Actualizar expiración
                 // $hours = (int) env('SANCTUM_EXPIRATION_HOURS', 2);
@@ -93,16 +91,16 @@ class AuthController extends Controller{
                     'token'      => $tokenResult->plainTextToken,
                     'expires_at' => $tokenResult->accessToken->expires_at == null ? null :  $tokenResult->accessToken->expires_at->toDateTimeString()
                 ];
-                return response()->json(["success"=>true,"data"=>$success, "message" => "User logged-in!"]);
+                return response()->json(["success" => true, "data" => $success, "message" => "User logged-in!"]);
             } else {
-                return response()->json(["success"=>false, "message" => "Unauthorised"],404);
+                return response()->json(["success" => false, "message" => "Unauthorised"], 404);
             }
         } else {
-            if(Auth::attempt(['name' => $request->name, 'password' => $request->password])){
+            if (Auth::attempt(['name' => $request->name, 'password' => $request->password])) {
                 $auth = Auth::user();
                 // return $auth;
                 //$tokenResult = $auth->createToken('LaravelSanctumAuth');
-                $tokenResult = $auth->createToken('LaravelSanctumAuth',['usuario']); // Asignar abilities al token
+                $tokenResult = $auth->createToken('LaravelSanctumAuth', ['usuario']); // Asignar abilities al token
 
                 // Actualizar expiración
                 // $hours = (int) env('SANCTUM_EXPIRATION_HOURS', 2);
@@ -115,9 +113,9 @@ class AuthController extends Controller{
                     'token'      => $tokenResult->plainTextToken,
                     'expires_at' => $tokenResult->accessToken->expires_at == null ? null :  $tokenResult->accessToken->expires_at->toDateTimeString()
                 ];
-                return response()->json(["success"=>true,"data"=>$success, "message" => "User logged-in!"]);
+                return response()->json(["success" => true, "data" => $success, "message" => "User logged-in!"]);
             } else {
-                return response()->json(["success"=>false, "message" => "Unauthorised"],404);
+                return response()->json(["success" => false, "message" => "Unauthorised"], 404);
             }
         }
     }
